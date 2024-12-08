476d42ca9a8942fe5b0802e06233f3e90fdd612c
ismetcan <icetejjr@gmail.com>
2024-12-08 16:39:38 +0300

    first

---
+++ README.md
@@ -0,0 +1,10 @@
+# Malaria Infection Prediction
+This project uses a Convolutional Neural Network (CNN) to predict malaria infection based on blood cell images. The model classifies images as either "Parasitized" (infected) or "Uninfected." The model is deployed via a Streamlit web app for easy use.
+
+## Data Source
+- [Malaria Dataset from Kaggle](https://www.kaggle.com/datasets/iarunava/cell-images-for-detecting-malaria)
+
+## Streamlit App
+- [Malaria Infection Prediction Web App](https://www.kaggle.com/datasets/iarunava/cell-images-for-detecting-malaria)
+
+
---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from tensorflow.keras.models import load_model
+from PIL import Image
+import numpy as np
+
+model = load_model("malaria_infection_cnn_model.h5")
+
+def process_image(img):
+    img = img.resize((60, 60))
+    img = img.convert("L")
+    img = np.array(img)
+    img = img / 255
+    img = np.expand_dims(img, axis=0)
+    return img
+
+st.title("Malaria Infection")
+st.write("Please upload an image to analyze whether it is parasitized or healthy.")
+
+file = st.file_uploader("Choose an Image", type=["jpg", "png", "jpeg"])
+
+if file is not None:
+    img = Image.open(file)
+    st.image(img, caption="Uploaded Image", use_container_width=True)
+    image = process_image(img)
+    prediction = model.predict(image)
+    predicted_class = np.argmax(prediction)
+    
+    class_names = ["Uninfected", "Parasitized"]
+    st.markdown(f"<h2 style='text-align: center;'>{class_names[predicted_class]}</h2>", unsafe_allow_html=True)
+
+
